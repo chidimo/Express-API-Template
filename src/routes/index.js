8239f17161cfcaff7b0eb3ab562ad86083793d88
Chidi Orji <orjichidi95@gmail.com>
2020-03-16 01:56:31 +0100

    chore: read environment variables

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
+import { testEnvironmentVariable } from '../settings';
 
 const indexRouter = express.Router();
-indexRouter.get('/', (req, res) => res.status(200).json({ message: 'Welcome to Express API template' }));
+indexRouter.get('/', (req, res) => res.status(200).json({ message: testEnvironmentVariable }));
 export default indexRouter;
