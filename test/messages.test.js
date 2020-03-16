3cf82c5c05e646a8e3579b5fceeae4fbf516a419
Chidi Orji <orjichidi95@gmail.com>
2020-03-16 15:13:28 +0100

    test: update test and ci-cd

---
+++ test/messages.test.js
@@ -0,0 +1,18 @@
+import { expect, server, BASE_URL } from './setup';
+
+describe('Messages', () => {
+  it('get messages page', done => {
+    server
+      .get(`${BASE_URL}/messages`)
+      .expect(200)
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body.messages).to.be.instanceOf(Array);
+        res.body.messages.forEach(m => {
+          expect(m).to.have.property('name');
+          expect(m).to.have.property('message');
+        });
+        done();
+      });
+  });
+});
