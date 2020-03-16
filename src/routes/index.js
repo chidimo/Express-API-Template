3e6db6968a4d324c2d987aff9fe22cc37cb9bfd5
Chidi Orji <orjichidi95@gmail.com>
2020-03-16 14:56:50 +0100

    feat: add messages route and controller

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import { indexPage } from '../controllers';
+import { indexPage, messagesPage } from '../controllers';
 
 const indexRouter = express.Router();
 indexRouter.get('/', indexPage);
+indexRouter.get('/messages', messagesPage);
 
 export default indexRouter;
