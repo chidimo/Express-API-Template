0f523f3c54a05602dcabcbcf9c113343660898f7
Chidi Orji <orjichidi95@gmail.com>
2020-03-21 02:13:55 +0100

    chore: add error handler

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import { indexPage, messagesPage, addMessage } from '../controllers';
-import { modifyMessage } from '../middleware';
+import { modifyMessage, performAsyncAction } from '../middleware';
 
 const indexRouter = express.Router();
 indexRouter.get('/', indexPage);
 indexRouter.get('/messages', messagesPage);
-indexRouter.post('/messages', modifyMessage, addMessage);
+indexRouter.post('/messages', modifyMessage, performAsyncAction, addMessage);
 
 export default indexRouter;
