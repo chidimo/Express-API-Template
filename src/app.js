18db2ad9335779f1d74d85ae22593f68621ea7e8
Chidi Orji <orjichidi95@gmail.com>
2020-03-21 02:13:55 +0100

    chore: add error handler

--- src/app.js
+++ src/app.js
@@ -11,4 +11,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use('/v1', indexRouter);
 
+app.use((err, req, res, next) => {
+  res.status(400).json({ error: err.stack });
+});
+
 export default app;
